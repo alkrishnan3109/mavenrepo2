c41f6b6f312597a9011444bb4d6aca0b05346fd7
alkrishnan3109 <alkrishnan1984@gmail.com>
2022-05-10 12:03:35 +0530

    removed unwanted functions

--- risebirdautomation/src/main/java/DriverInit/BrowserSelection.java
+++ risebirdautomation/src/main/java/DriverInit/BrowserSelection.java
@@ -13,29 +13,5 @@ public class BrowserSelection
 		System.out.println("selectBrowser");
 	}
 	
-	public void selectBrowser(String browsername)
-	{
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		
-		System.out.println("selectBrowser");
-	}
-	
-	public void selectBrowser(String browsername,String browsername1)
-	{
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		
-		
-	}
-	
-	public void selectBrowser(int browsername,String browsername2)
-	{
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		System.out.println("selectBrowser");
-		
-			}
+	 
 }
